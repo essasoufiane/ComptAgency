65e5506c5b0f01295930801617b8269d70821726
Soufiane Essaouari <soufiane.pro@yahoo.com>
2022-08-07 18:03:43 +0200

    Confirmation password + assert

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -4,48 +4,68 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 
 class RegistrationFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('lastname')
-            ->add('firstname')
-            ->add('phone')
-            ->add('email')
+            ->add('lastname', TextType::class, [
+                "label" => "Nom",
+                "attr" => ["placeholder" => "Kim"]
+            ])
+            ->add('firstname', TextType::class, [
+                "label" => "Prénom",
+                "attr" => ["placeholder" => "Jong"]
+            ])
+            ->add('phone', IntegerType::class, [
+                "label" => "Tel",
+                "attr"  => ["placeholder" => "07 09 09 90 88"]
+            ])
+            ->add('email', EmailType::class, [
+                "label" => "Email",
+                "attr"  => ["placeholder" => "exemple@exemple.com"]
+            ])
             ->add('agreeTerms', CheckboxType::class, [
+                "label" => "Accepter les conditions",
                 'mapped' => false,
                 'constraints' => [
                     new IsTrue([
-                        'message' => 'You should agree to our terms.',
+                        'message' => 'Vous devez accepter les conditions générales.',
                     ]),
                 ],
             ])
-            ->add('plainPassword', PasswordType::class, [
-                // instead of being set onto the object directly,
-                // this is read and encoded in the controller
-                'mapped' => false,
-                'attr' => ['autocomplete' => 'new-password'],
-                'constraints' => [
-                    new NotBlank([
-                        'message' => 'Please enter a password',
-                    ]),
-                    new Length([
-                        'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
-                        // max length allowed by Symfony for security reasons
-                        'max' => 4096,
-                    ]),
-                ],
-            ])
+            ->add('password', RepeatedType::class,[
+
+                
+                'type'=>PasswordType::class,
+               
+                'invalid_message'=>'Le mot de passe et la confirmation doit être identique', 
+                'label'=>'Confirmez votre mot de passe',
+                
+                'required'=> true, 
+                'first_options'=>[
+                'label'=>'Mot de passe',
+                'attr'=>[
+                    'placeholder'=>'Merci de saisir votre mot de passe.']], 
+                'second_options'=>['label'=>'Confirmez votre mot de passe',
+                
+                'attr'=>[
+                    'placeholder'=>'Merci de confirmer votre mot de passe.']
+                ]
+                
+                ])
         ;
     }
 
