e4094bcf3ff122ef64ab64a532a81343fa6d8d4d
Soufiane Essaouari <soufiane.pro@yahoo.com>
2022-08-22 03:43:06 +0200

    + entity article + crud

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -43,9 +43,13 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\OneToMany(mappedBy: 'user', targetEntity: Entreprise::class, orphanRemoval: true)]
     private Collection $entreprises;
 
+    #[ORM\OneToMany(mappedBy: 'admin', targetEntity: Article::class)]
+    private Collection $articles;
+
     public function __construct()
     {
         $this->entreprises = new ArrayCollection();
+        $this->articles = new ArrayCollection();
     }
 
     public function getId(): ?int
@@ -197,6 +201,36 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
             // set the owning side to null (unless already changed)
             if ($entreprise->getUser() === $this) {
                 $entreprise->setUser(null);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Article>
+     */
+    public function getArticles(): Collection
+    {
+        return $this->articles;
+    }
+
+    public function addArticle(Article $article): self
+    {
+        if (!$this->articles->contains($article)) {
+            $this->articles[] = $article;
+            $article->setAdmin($this);
+        }
+
+        return $this;
+    }
+
+    public function removeArticle(Article $article): self
+    {
+        if ($this->articles->removeElement($article)) {
+            // set the owning side to null (unless already changed)
+            if ($article->getAdmin() === $this) {
+                $article->setAdmin(null);
             }
         }
 
