1f95bbc058a83b8140419be3fd9d1107cd523a9b
Soufiane Essaouari <soufiane.pro@yahoo.com>
2022-08-25 07:57:45 +0200

    + section presentation

--- src/Form/ContactType.php
+++ src/Form/ContactType.php
@@ -10,6 +10,8 @@ use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 
 class ContactType extends AbstractType
 {
