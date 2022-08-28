253a4d515b1f3f65f7069936d99c6f67b7548bc6
Soufiane Essaouari <100834396+essasoufiane@users.noreply.github.com>
2022-08-28 17:49:59 +0200

    FIX UserType

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -12,6 +12,7 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 
 class UserType extends AbstractType
 {
